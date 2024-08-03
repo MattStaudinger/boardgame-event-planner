1a25e13358324a766c56764310dd9d4069310b3b
Matthias Privat <matthiasprivat@matthias.staudinger>
2024-08-03 19:37:59 +0200

    test mobile design

--- app/[eventId]/join/page.tsx
+++ app/[eventId]/join/page.tsx
@@ -27,22 +27,22 @@ export default async function JoinEvent({ params }: EventProps) {
       <h1 className="text-custom-green text-2xl mt-12 font-bold">
         Join on {dayjs(event.date).format("dddd, DD.MM.YYYY")}
       </h1>
-      <div className="sm:px-2 px-6">
-        {isJoiningWaitingList && (
-          <p className="text-sm/6 text-black/50 mb-[16px]">
-            The event is already full... You are joining the waiting list and
-            will be notified via email if a spot becomes available.
-          </p>
-        )}
-        <Form event={event} />
+      {/* <div className="sm:px-2 px-6"> */}
+      {isJoiningWaitingList && (
+        <p className="text-sm/6 text-black/50 mb-[16px]">
+          The event is already full... You are joining the waiting list and will
+          be notified via email if a spot becomes available.
+        </p>
+      )}
+      <Form event={event} />
 
-        <Link
-          href={`/${event.id}`}
-          className="rounded flex  justify-center font-semibold w-full bg-black/30 py-2 px-8 text-md text-white data-[hover]:bg-custom-green-hover data-[active]:bg-custom-green-hover"
-        >
-          Back
-        </Link>
-      </div>
+      <Link
+        href={`/${event.id}`}
+        className="rounded flex  justify-center font-semibold w-full bg-black/30 py-2 px-8 text-md text-white data-[hover]:bg-custom-green-hover data-[active]:bg-custom-green-hover"
+      >
+        Back
+      </Link>
+      {/* </div> */}
     </>
   )
 }
--- components/NavBackButton.tsx
+++ components/NavBackButton.tsx
@@ -8,7 +8,7 @@ type NavBackButtonProps = {
 export default async function NavBackButton({ route }: NavBackButtonProps) {
   return (
     <Link href={route}>
-      <TiDelete className="text-black/50 w-[30px] h-[30px] absolute right-[16px]" />
+      <TiDelete className="text-black/50 w-[30px] h-[30px] absolute right-[16px] top-[16px]" />
     </Link>
   )
 }
